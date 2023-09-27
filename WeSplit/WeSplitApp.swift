7d68f8af5a28e50b4e200958a54d0b4b170df062
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-09-27 12:29:08 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aaron Brown on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
